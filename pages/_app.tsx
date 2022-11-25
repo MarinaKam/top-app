d0872770529eb87c99afc1da9b7c5fa369d8a22f
Maryna Kamysheva <marina.stozhok@gmail.com>
2022-11-25 15:41:12 +0200

    feat: Initial settings.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
-}
+};
